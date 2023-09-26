1e7db0e1ae16c1b764a89ec75b2db6f5635daf49
asifaziz969 <asif969aziz@gmail.com>
2023-09-26 21:21:14 +0500

    sencond chalange done i have added paragraph using ejs

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
-const express = require('express')
-const bodyparser = require('body-parser')
-const app = express()
-const ejs = require ("ejs");
-app.set('view engine', 'ejs');
-app.use(bodyparser.urlencoded({extended: true}));
+const express = require("express");
+const bodyparser = require("body-parser");
+const app = express();
+const ejs = require("ejs");
+app.set("view engine", "ejs");
+app.use(bodyparser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+const port = 3000;
 
-const port = 3000
+var cont = "i go to office , i play golf and i eat , code sleep and repeat ";
 
-app.get('/', (req, res) => {
-  res.render("index")
-})
+app.get("/", (req, res) => {
+  res.render("index", { content: cont });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
